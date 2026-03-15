51fb301f5bd5fa661656e23b1aca371f479cd23e
Aniruktha <23pw01@psgtech.ac.in>
2026-03-15 15:42:58 +0530

    deployment changes railway

--- moodle/settings.py
+++ moodle/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = 'django-insecure-gc+z@p90umwalz_1j(p+09n(94$x^!-r9%66met6b=v3*fflp1
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.vercel.app', '.vercel.dev', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ["*"]
 # Application definition
 
 INSTALLED_APPS = [
