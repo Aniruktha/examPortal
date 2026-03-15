e3fc7d375022634474bfd18ee4cdb69d799802fc
Aniruktha <23pw01@psgtech.ac.in>
2026-03-15 14:43:30 +0530

    deployment changes

--- api/index.py
+++ api/index.py
@@ -4,6 +4,10 @@ import sys
 # Add project to path
 sys.path.insert(0, os.path.join(os.path.dirname(__file__), '..'))
 
+# Install pymysql as MySQLdb (before importing Django)
+import pymysql
+pymysql.install_as_MySQLdb()
+
 # Set Django settings
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'moodle.settings')
 
